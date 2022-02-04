88ece2919eb81ff59396296b4f5a3ebc0bb45519
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2022-02-04 17:26:48 +0530

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -1,6 +1,6 @@
 # Insert bot token here
-api_token = "BOT_TOKEN"
+api_token = "5076774553:AAGjrGI_Dq3e1IqBY1AwdKJBzDs_q3YdrJY"
 
 
 # Replace admin_link with your username (https://t.me/username)
-admin_link = "https://t.me/12345" 
+admin_link = "https://t.me/penorpen" 
