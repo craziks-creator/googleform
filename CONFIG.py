44304bfcfa33af319c922fb0c00397894b560dba
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2022-02-14 18:55:17 +0530

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -3,4 +3,4 @@ api_token = "5076774553:AAGjrGI_Dq3e1IqBY1AwdKJBzDs_q3YdrJY"
 
 
 # Replace admin_link with your username (https://t.me/username)
-admin_link = "https://t.me/The_Pundits" 
+admin_link = "https://t.me/the_whitedevil" 
